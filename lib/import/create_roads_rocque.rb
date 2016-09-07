417c16fb62921392edfecdcd3cf84abd8123dab3
Ian Johnson <enjalot@gmail.com>
2016-09-06 21:08:03 -0700

    update data source for rocque

--- lib/import/create_roads_rocque.rb
+++ lib/import/create_roads_rocque.rb
@@ -5,7 +5,8 @@ module Import
     def up
       #shapefile = "rocquev2/rocque_segments.shp"
       #shapefile = "data/geo/roads/rocque/rocque/Rocque_QA_final.shp"
-      shapefile = "data/geo/roads/rocque/rocque/roc_lines.shp"
+      #shapefile = "data/geo/roads/rocque/rocque/roc_lines.shp"
+      shapefile = "data/geo/roads/rocque/rocqueftlwc/Rocque_QA_final_FTL_WC.shp"
       importfile = "data/geo/roads/rocque/importer.sql"
       dbname = "kl_development"
       tablename = "roads_rocque"
